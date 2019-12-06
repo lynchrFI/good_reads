83c9bfadc3c11ee90bf9f39a3cc51ac3cc0d7f37
Ryan Lynch <lynchrbusiness@gmail.com>
2019-12-06 01:49:13 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in good_reads.gemspec
+gemspec
---
+++ lib/good_reads.rb
@@ -0,0 +1,6 @@
+require "good_reads/version"
+
+module GoodReads
+  class Error < StandardError; end
+  # Your code goes here...
+end
